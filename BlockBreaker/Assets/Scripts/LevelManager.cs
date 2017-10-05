e1fff033dcda1c9a95ddedbd7ea0df9c9b88741f
Unknown <green.chad22@gmail.com>
2017-10-05 11:15:52 -0600

    BBSchoolUpdated

--- BlockBreaker/Assets/Scripts/LevelManager.cs
+++ BlockBreaker/Assets/Scripts/LevelManager.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour {
+
+
+	public void LevelLoad (string lvl) {
+		SceneManager.LoadScene (lvl);
+	}
+
+
 
 	public void ExitGame () {
 		print ("Tried to Exit.");
