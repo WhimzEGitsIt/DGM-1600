9551b4d10e78a927e8de6e62f7a8657a13d2f1c1
Unknown <green.chad22@gmail.com>
2017-10-12 11:21:08 -0600

    BlockBUpdatedSchool

---
+++ BlockBreaker/Assets/Scripts/BallDamage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDamage : MonoBehaviour {
+
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		var hit = collision.gameObject;
+		var health = hit.GetComponent<BlockHealth> ();
+		if (health != null) 
+		{
+			health.TakeDamage (1);
+		}
+
+		Destroy (gameObject);
+	}
+
+}
---
+++ BlockBreaker/Assets/Scripts/BlockHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockHealth : MonoBehaviour {
+
+	public const int maxHealth = 1;
+	public int currentHealth =maxHealth;
+
+	public void TakeDamage (int amount) {
+		currentHealth -= amount;
+		if (currentHealth <= 0) 
+		{
+			currentHealth = 0;
+			print ("Dead");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
