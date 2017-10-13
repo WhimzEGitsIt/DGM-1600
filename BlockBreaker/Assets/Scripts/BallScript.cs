3d75944826f79734588febfb8dd169990dfeb4f6
Unknown <green.chad22@gmail.com>
2017-10-12 20:03:39 -0600

    UpdatedAtHome

---
+++ BlockBreaker/Assets/Scripts/BallScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+	float BallForce = 200f;
+	// Use this for initialization
+	void Start () {
+		Rigidbody ballrig = GetComponent<Rigidbody> ();
+		ballrig.AddForce (0, BallForce, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	public float paddleSpeed = 10f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate (paddleSpeed * Time.deltaTime * Input.GetAxis ("Horizontal"), 0, 0);
+		float xPos = transform.position.x + (Input.GetAxis ("Horizontal") * paddleSpeed);
+		var pos = transform.position;
+		pos.x = Mathf.Clamp (xPos, -13.5f, 13.5f);
+		transform.position = pos;
+	}
+
+	void OnCollisionEnter (Collision col) {
+		foreach (ContactPoint contact in col.contacts) {
+			if (contact.thisCollider == GetComponent<BoxCollider>()) {
+				float calc = contact.point.x - transform.position.x;
+
+				contact.otherCollider.GetComponent<Rigidbody> ().AddForce (400f * calc, 0, 0);
+			}
+		}
+	}
+
+}
+
