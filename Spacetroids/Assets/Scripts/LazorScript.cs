f4ad74aced9f5bc4970d8e282f9c47a7d1246224
Unknown <green.chad22@gmail.com>
2017-11-07 11:17:25 -0700

    SpaceUpdateLAZORS

---
+++ Spacetroids/Assets/Scripts/LazorScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazorScript : MonoBehaviour {
+
+
+	public float lifetime;
+	public float speed;
+
+
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+	
+
+		lifetime -= Time.deltaTime;
+		if (lifetime <= 0) {
+			Destroy (this.gameObject);
+		}
+
+		//transform.Translate (Vector3.up * speed * Time.deltaTime);
+
+	}
+}
--- Spacetroids/Assets/Scripts/PlayerController.cs
+++ Spacetroids/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	public GameObject projectile;
+	public Transform shotPos;
+
+
 	public float movementSpeed = 5.0f;
-	public float clockwise = 100.0f;
-	public float counterClockwise = -100.0f;
+	public float clockwise = 50.0f;
+	public float counterClockwise = -50.0f;
+	public float shotForce;
 
 	void Start () {
 		 
@@ -27,11 +32,21 @@ public class PlayerController : MonoBehaviour {
 			transform.position += Vector3.right * Time.deltaTime * movementSpeed;
 		}
 
-		if (Input.GetKey (KeyCode.E)) {
+		if (Input.GetKey (KeyCode.Q)) {
 			transform.Rotate (0, 0, Time.deltaTime * clockwise);
-		} else if (Input.GetKey (KeyCode.Q)) {
+		} else if (Input.GetKey (KeyCode.E)) {
 			transform.Rotate (0, 0, Time.deltaTime * counterClockwise);
 		}
+
+
+		if (Input.GetButtonUp ("Fire1")) 
+		{
+			GameObject shot = Instantiate (projectile, shotPos.position, shotPos.rotation) as GameObject;
+			shot.GetComponent<Rigidbody2D> ().AddForce (shotPos.up * shotForce);
+			//shot.AddFroce(shotPos.forward * shotForce);
+		}
+	
+
 	}
 
 }
