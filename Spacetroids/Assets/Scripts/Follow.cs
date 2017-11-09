ed993b22c437c1aee0db8e5559a74798eaebf6eb
Unknown <green.chad22@gmail.com>
2017-11-09 11:19:43 -0700

    SchoolGameUpdate

---
+++ Spacetroids/Assets/Scripts/Follow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+
+	public Transform objectToFollow;
+	public float zOffset;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 myPos = objectToFollow.position;
+
+		myPos.z = zOffset;
+
+		gameObject.transform.position = myPos;
+
+	}
+}
--- Spacetroids/Assets/Scripts/PlayerController.cs
+++ Spacetroids/Assets/Scripts/PlayerController.cs
@@ -8,9 +8,9 @@ public class PlayerController : MonoBehaviour {
 	public Transform shotPos;
 
 
-	public float movementSpeed = 5.0f;
-	public float clockwise = 50.0f;
-	public float counterClockwise = -50.0f;
+	public float movementSpeed = 100.0f;
+	public float clockwise = 1.0f;
+	public float counterClockwise = -1.0f;
 	public float shotForce;
 
 	void Start () {
@@ -20,21 +20,21 @@ public class PlayerController : MonoBehaviour {
 
 	void Update () {
 		if (Input.GetKey (KeyCode.W)) {
-			transform.position += Vector3.up * Time.deltaTime * movementSpeed;
+			transform.Translate (Vector3.up * Time.deltaTime * movementSpeed);
 		}
 		if (Input.GetKey (KeyCode.S)) {
-			transform.position += Vector3.down * Time.deltaTime * movementSpeed;
+			transform.Translate (-Vector3.up * Time.deltaTime * movementSpeed);
 		}
-		if (Input.GetKey (KeyCode.A)) {
-			transform.position += Vector3.left * Time.deltaTime * movementSpeed;
-		} 
-		else if (Input.GetKey (KeyCode.D)) {
-			transform.position += Vector3.right * Time.deltaTime * movementSpeed;
-		}
+		//if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (Vector3.left * Time.deltaTime * movementSpeed);
+		//} 
+		//else if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (Vector3.right * Time.deltaTime * movementSpeed);
+		//}
 
-		if (Input.GetKey (KeyCode.Q)) {
+		if (Input.GetKey (KeyCode.A)) {
 			transform.Rotate (0, 0, Time.deltaTime * clockwise);
-		} else if (Input.GetKey (KeyCode.E)) {
+		} else if (Input.GetKey (KeyCode.D)) {
 			transform.Rotate (0, 0, Time.deltaTime * counterClockwise);
 		}
 
